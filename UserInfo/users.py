d8511afe3c3ed24ae2f6d4f27104f2f9b9a9a05c
Forrest Sun <sxh19911230@gmail.com>
2015-01-25 00:52:25 -0800

    Added a fuction

--- UserInfo/users.py
+++ UserInfo/users.py
@@ -1,11 +1,14 @@
 """ This is for User accessing
-	databaseCheck() is used to check if database works. 0 if no, 1 if yes
+	databaseCheck() is used to check if database works. 0 if no, 1 if yes. No exception thrown out.
 	getID() is used to login. it returns ID if username and password matches; return -1 id no username; return -2 if password is wrong
+	getIdByEmail() is used to get User's Id by its email
 	getInfo() is used to get all the information for a student and return into a tuple
-	Register() is used to register. There are several precautions to use it:
-	1. username is primary, which means we don't want 2 users have the same username. Be sure to check it for the register part.
-	2. 
 	
+	Register() is used to register. There are several precautions to use it:
+	1. username and email are primary, which means we don't want 2 users have the same username or email. Be sure to check it for the register part.
+	       Use getID() and getIdByEmail()
+	
+	2. I haven't think about it
 """
 
 import mysql.connector
@@ -15,18 +18,8 @@ import config
 
 
 def getID(username, password):
-	try:
-	 #Info to access database
-	  cnx = mysql.connector.connect(**config.config)  #connect database
-	  
-	except mysql.connector.Error as err:
-	  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-		print("Something is wrong with your user name or password")
-	  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-		print("Database does not exists")
-	  else:
-		print(err)
-	  exit(0)
+	
+	cnx = mysql.connector.connect(**config.config)
 	  
 	cursor = cnx.cursor()
 
@@ -48,21 +41,29 @@ def getID(username, password):
 	#Password is Wrong
 	return -2	
 
+def getIdByEmail(email):
+	cnx = mysql.connector.connect(**config.config)
+	  
+	cursor = cnx.cursor()
+
+	cursor.execute("SELECT ID FROM `users` WHERE email_address='%s';" % email)
+	user_info = cursor.fetchall()
+	
+	cursor.close()
+	cnx.close()
+	
+	
+	#No username
+	if len(user_info) == 0:
+		return -1
+	return user_info[0][0]
+		
 
 def getInfo(ID):
-	try:
-	 #Info to access database
-	  cnx = mysql.connector.connect(**config.config)  #connect database
-	  
-	except mysql.connector.Error as err:
-	  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-		print("Something is wrong with your user name or password")
-	  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-		print("Database does not exists")
-	  else:
-		print(err)
-	  exit(0)
-	  
+
+	 
+	cnx = mysql.connector.connect(**config.config)  #connect database
+
 	cursor = cnx.cursor()
 	cursor.execute("SELECT * FROM `users` WHERE ID='%s';" % ID)
 	user_info = cursor.fetchall()
@@ -77,19 +78,9 @@ def getInfo(ID):
 	return user_info[0]
 
 def Register(username, password, email=None, major=None):
-	try:
-	 #Info to access database
-	  cnx = mysql.connector.connect(**config.config)  #connect database
-	  
-	except mysql.connector.Error as err:
-	  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-		print("Something is wrong with your user name or password")
-	  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-		print("Database does not exists")
-	  else:
-		print(err)
-	  exit(0)
-	  
+
+	cnx = mysql.connector.connect(**config.config)  #connect database
+
 	cursor = cnx.cursor()
 	
 	if email:
--- UserInfo/userstest.py
+++ UserInfo/userstest.py
@@ -4,5 +4,7 @@ print "Get user ID by username and password"
 print users.getID("sxh19911230", "P@ssw0rd")
 print "Get user info by ID"
 print users.getInfo(1)
+print "sxh19911230@gmail.com's ID:"
+print users.getIdByEmail("sxh19911230@gmail.com")
 print "Register"
-users.Register("hehe8","qwe",None,None)
+users.Register("hehe9","qwe",None,None)
