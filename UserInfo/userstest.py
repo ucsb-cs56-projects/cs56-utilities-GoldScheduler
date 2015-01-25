5b2a9a2b3445c74b39cd1abb64cc95e455b264c1
Forrest Sun <sxh19911230@gmail.com>
2015-01-24 23:40:09 -0800

    Added User Registration API

---
+++ UserInfo/userstest.py
@@ -0,0 +1,8 @@
+import users
+
+print "Get user ID by username and password"
+print users.getID("hehe3", "qwe")
+print "Get user info by ID"
+print users.getInfo(8)
+print "Register"
+users.Register("hehe5","qwe",None,None)
