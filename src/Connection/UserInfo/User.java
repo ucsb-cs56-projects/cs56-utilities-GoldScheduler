7b443dcbab7ec61407c4008b23b41087cdc0347d
Wesley Pollek <wpollek@engineering.ucsb.edu>
2015-02-07 12:12:32 -0800

    Added Setters, Getters and basic Constructors

--- src/Connection/UserInfo/User.java
+++ src/Connection/UserInfo/User.java
@@ -4,10 +4,31 @@ package Connection.UserInfo;
 
 /**
  * 
- * @author 
+ * @author Wesley Pollek, Forrest Sun
  *
  */
 
 public class User {
-	//TODO
+	private String username;
+	private String password;
+	private String email;
+	private String major;
+	public User(){
+		username=password=email=major=NULL;
+	}
+	public User(String u, String p, String e, String m){
+		username=u;
+		password=p;
+		email=e;
+		major=m;
+	}
+	public String getUsername(){return this.username}
+	public String getPassword(){return this.password}
+	public String getEmail(){return this.email}
+	public String getMajor(){return this.major}
+	public void setUsername(string username){this.username=username;}
+	public void setPassword(string password){this.password=password:}
+	public void setEmail(string email){this.email=email;}
+	public void setMajor(string major){this.major=major;}
+
 }
