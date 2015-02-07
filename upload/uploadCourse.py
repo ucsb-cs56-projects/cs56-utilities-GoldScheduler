46b9173bdb3ada8fee141896b8e81713b0a46b7b
Forrest Sun <sxh19911230@gmail.com>
2015-02-07 01:28:54 -0800

    Java API for User and Upload file and all course Info has beed uploaded

---
+++ upload/uploadCourse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import mysql.connector
+from mysql.connector import errorcode
+import io
+import re
+
+from UserInfo.config import config
+
+f = open('output/course_catalog.txt', 'r')
+
+cnx = mysql.connector.connect(**config)
+cursor = cnx.cursor()
+for line in f:
+	m = re.match('([\w+\s]+)\s(\d\w+)\s\-\s(.+)', line)
+	if m:
+		dept = m.group(1)
+		code = m.group(2)
+		dscp = m.group(3)
+
+		cursor.execute("INSERT INTO courses (course_name, course_code, department, description) VALUES ('%s','%s','%s','%s');" % ("".join(dept.split())+code, code, dept, dscp.replace("\\","\\\\").replace("'","\\'")))
+
+		
+
+cnx.commit()
+cursor.close()
+cnx.close()
