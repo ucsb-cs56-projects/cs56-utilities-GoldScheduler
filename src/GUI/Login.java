48b848082c447f858d58246c52198b2c2f32f9d7
Wesley Pollek <wpollek@umail.ucsb.edu>
2015-02-10 13:57:59 -0800

    added to Login Page

--- src/GUI/Login.java
+++ src/GUI/Login.java
@@ -4,30 +4,66 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.ArrayList;
+import Connection.UserInfo.*;
 
-public class Login extends JFrame {
-    JPanel loginBox = new JPanel(new BorderLayout());
-    JTextField username= new JTextField(20);    
-    JLabel userLable= new JLabel("Enter Username:");
-    JTextField password= new JTextField(20);
-    JLabel passLable= new JLabel("Enter Password:");
-    public Login(){
-        super("Enter Login Information");
-        setSize(400,400);
-        setDefaultCloseOperation(JFrame. EXIT_ON_CLOSE);
-        setBackground(Color.YELLOW);
-        loginBox.add(userLable, BorderLayout.WEST);
-        loginBox.add(username, BorderLayout.CENTER);
-        loginBox.add(passLable, BorderLayout.WEST);
-        loginBox.add(password, BorderLayout.CENTER);
-        loginBox.setBackground(Color.BLUE);
-        loginBox.setBorder(BorderFactory.createTitledBorder(
-                BorderFactory.createEtchedBorder(), "Login Panel"));
-        add(loginBox);
-        loginBox.pack();
+public class Login extends JPanel{
+    JTextField username= new JTextField(20);
+    JLabel userLabel= new JLabel("Enter Username:");
+    JPasswordField password= new JPasswordField(20);
+    JLabel passLabel= new JLabel("Enter Password:");
+    JButton login= new JButton("Login");
+    login.addActionListener(new Validator());
+    JButton forpass = new JButton ("Forgot Password?");
+    JLabel wrong = new JLabel();
+    class Validator implements ActionListener{
+	public void actionPerformed(ActionEvent e){
+	    String userinfo = username.getText();
+	    String passinfo = password.getText();
+	    int ID = getID(userinfo,passinfo);
+	    if(ID == -3) wrong.setText("No Connection");
+	    else if (ID==-2) wrong.setText("Incorrect Password");
+	    else if (ID==-1) wrong.setText("No Username");
+	    
+	}
     }
     public static void main (String[] args){
-        Login frame = new Login();
-        frame.setVisible(true);
+       	JFrame window = new JFrame();
+	Login frame = new Login();
+	window.setContentPane(frame);
+	window.setSize(720,720);
+        window.setDefaultCloseOperation(JFrame. EXIT_ON_CLOSE);
+	window.setVisible(true);
+        frame.go();
+    }
+    public void go(){
+	GridLayout basegrid = new GridLayout(3, 3);
+	this.setLayout(basegrid);
+	JPanel[] rows = new JPanel[3];
+	for(int i=0;i<3; i++){
+		rows[i]= new JPanel();
+		rows[i].setOpaque(true);
+		rows[i].setBackground(Color.BLUE);
+		this.add(rows[i]);
+	}
+	GridLayout innergrid = new GridLayout(4, 4);
+	JPanel innerpanel = new JPanel();
+	innerpanel.setLayout(innergrid);
+	JPanel[][] spot= new JPanel[4][4];
+	for(int m=0;m<4;m++){
+	    for (int k=0;k<4;k++){
+		spot[m][k]=new JPanel();
+		spot[m][k].setBackground(Color.YELLOW);
+		spot[m][k].setOpaque(true);
+		innerpanel.add(spot[m][k]);
+	    }
+	}
+	spot[1][1].add(userLabel);
+	spot[1][2].add(username);
+	spot[2][1].add(passLabel);
+	spot[2][2].add(password);
+	spot[3][1].add(login);
+	spot[3][2].add(forpass);
+	//spot[0][1].add(wrong);
+	rows[1].add(innerpanel);
     }
 }
