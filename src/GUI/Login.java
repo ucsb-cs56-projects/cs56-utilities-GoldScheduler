2f55daeae2d50e6fd8059494070e0e213ca11a02
Wesley Pollek <wpollek@engineering.ucsb.edu>
2015-02-09 17:40:22 -0800

    Update to Login page

--- src/GUI/Login.java
+++ src/GUI/Login.java
@@ -3,5 +3,29 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.ArrayList;
 
-public class Login extends JPanel implements ActionListener{
-    
+public class Login extends JFrame {
+    JPanel loginBox = new JPanel(new BorderLayout());
+    JTextField username= new JTextField(20);    
+    JLabel userLable= new JLable("Enter Username:");
+    JTextField password= new JTextField(20);
+    JLabel passLable= new JLable("Enter Password:");
+    loginBox.add(userLable, BorderLayout.WEST);
+    loginBox.add(username, BorderLayout.CENTER);
+    loginBox.add(passLable, BorderLayout.WEST);
+    loginBox.add(password, BorderLayout.CENTER);
+    loginBox.setBackgroundColor(Color.BLUE);
+    loginBox.setBorder(BorderFactory.createTitledBorder(
+                BorderFactory.createEtchedBorder(), "Login Panel"));
+    public Login(){
+            super("Enter Login Information");
+            setSize(400,400);
+            setDefaultCloseOperation(JFrame. EXIT_ON_CLOSE);
+            setBackground(Color.YELLOW)
+            add(loginBox);
+            pack();
+    }
+    public static void main (String[] args){
+        Login frame = new Login();
+        frame.setVisible(true);
+    }
+}
