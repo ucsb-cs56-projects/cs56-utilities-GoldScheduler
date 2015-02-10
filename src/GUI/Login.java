884d8dde4c202bf94acd250713679489b6fa0040
Forrest Sun <sxh19911230@gmail.com>
2015-02-10 08:07:46 -0800

    syntax error

--- src/GUI/Login.java
+++ src/GUI/Login.java
@@ -1,3 +1,5 @@
+package GUI;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -6,19 +8,19 @@ import java.util.ArrayList;
 public class Login extends JFrame {
     JPanel loginBox = new JPanel(new BorderLayout());
     JTextField username= new JTextField(20);    
-    JLabel userLable= new JLable("Enter Username:");
+    JLabel userLable= new JLabel("Enter Username:");
     JTextField password= new JTextField(20);
-    JLabel passLable= new JLable("Enter Password:");
+    JLabel passLable= new JLabel("Enter Password:");
     public Login(){
         super("Enter Login Information");
         setSize(400,400);
         setDefaultCloseOperation(JFrame. EXIT_ON_CLOSE);
-        setBackground(Color.YELLOW)
+        setBackground(Color.YELLOW);
         loginBox.add(userLable, BorderLayout.WEST);
         loginBox.add(username, BorderLayout.CENTER);
         loginBox.add(passLable, BorderLayout.WEST);
         loginBox.add(password, BorderLayout.CENTER);
-        loginBox.setBackgroundColor(Color.BLUE);
+        loginBox.setBackground(Color.BLUE);
         loginBox.setBorder(BorderFactory.createTitledBorder(
                 BorderFactory.createEtchedBorder(), "Login Panel"));
         add(loginBox);
