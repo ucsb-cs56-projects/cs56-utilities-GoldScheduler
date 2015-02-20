b5d7013de05c7f1de302ef00b80584c04099baa3
Forrest Sun <sxh19911230@gmail.com>
2015-02-19 22:52:23 -0800

    Added GEFullFill

--- src/connection/courseInfo/CourseConnection.java
+++ src/connection/courseInfo/CourseConnection.java
@@ -55,7 +55,7 @@ public class CourseConnection extends GolderConnection{
 		ArrayList<Course> ca = new ArrayList<Course>();
 
 		
-			if (stmt.execute(String.format("SELECT * FROM `courses` WHERE description LIKE '%%%s%%';", s))) {
+		if (stmt.execute(String.format("SELECT * FROM `courses` WHERE description LIKE '%%%s%%';", s))) {
 				rs = stmt.getResultSet();
 		        
 		        
@@ -78,19 +78,21 @@ public class CourseConnection extends GolderConnection{
 			        		
 			        		Lecture q = new Lecture(rsL.getString("instructor_name"), rsL.getInt("start_time"),  rsL.getInt("end_time"), deCodeWeek(rsL.getInt("week")), "", "", new Color(169,226,195));
 			                Course r = new Course(rs.getString("course_name"), rs.getString("description"), rs.getString("description"),
-			                		rs.getString("department"), rs.getString("units"), new Course [0], new String [0], new String[0], q);
+			                		rs.getString("department"), rs.getString("units"), new Course [0], new String [0], 
+			                		deCodeGEFill(rs.getInt(5), rs.getString(6), rs.getInt(7), rs.getInt(8), rs.getInt(9), rs.getInt(10), rs.getInt(11), rs.getInt(12), rs.getInt(13), rs.getString(14), rs.getInt(15), rs.getInt(16))
+			                		, q);
 			        		
 			        		ca.add(r);
 			        	}
-			        	
+			        	rsL.close();
 			        }
 			        
 		        }
 		        
-			}
-			
+		}
 			
 		
+		stmt2.close();
 		
 		return ca;
 		
@@ -137,7 +139,7 @@ public class CourseConnection extends GolderConnection{
 						break;
 					case 6:
 						week[week.length-count] = 'U';
-						break;
+						break;	
 					}
 					count--;
 				}
@@ -147,6 +149,36 @@ public class CourseConnection extends GolderConnection{
 		
 		
 		return week;
+		
+	}
+	
+	public static String[] deCodeGEFill(int b, String c, int d, int e, int f, int g, int h, int ethnic, int euro, String quantitative, int world_culture, int writ) {
+		
+		int count = 0;
+		System.out.println(""+ b + d + e + f + g + h + ethnic + euro + world_culture +writ);
+		count = b + d + e + f + g + h + ethnic + euro + world_culture +writ;
+		
+		if (!c.equals("0")) count ++;
+		if (!quantitative.equals("0")) count ++;
+		
+		String [] req = new String[count];
+		
+		if (b == 1) {req[req.length-count] = "B"; count--;}
+		if (c.equals("1")) {req[req.length-count] = "C"; count--;}
+		else if (!c.equals("0")) {req[req.length-count] = "C (must take both this course and " + c + " to statisfy)"; count--;}
+		if (d == 1) {req[req.length-count] = "D"; count--;}
+		if (e == 1) {req[req.length-count] = "E"; count--;}
+		if (f == 1) {req[req.length-count] = "F"; count--;}
+		if (g == 1) {req[req.length-count] = "G"; count--;}
+		if (h == 1) {req[req.length-count] = "H"; count--;}
+		if (ethnic == 1) {req[req.length-count] = "ethnic".toUpperCase(); count--;}
+		if (euro == 1) {req[req.length-count] = "euro".toUpperCase(); count--;}
+		if (quantitative.equals("1")) {req[req.length-count] = "QUANTITATIVE"; count--;}
+		else if (!quantitative.equals("0")) {req[req.length-count] = "QUANTITATIVE (must take both this course and " + c + " to statisfy)"; count--;}
+		if (world_culture == 1) {req[req.length-count] = "world_culture".toUpperCase(); count--;}
+		if (writ == 1) {req[req.length-count] = "writ".toUpperCase(); count--;}
+		
+		return req;
 		
 	}
 	
