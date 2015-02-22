9b362eae655040737a7cf7e5a181f4ae6e11deef
Chloe Pounds <chloepounds@mailshell.com>
2015-02-21 16:41:00 -0800

    WIP: Began implementing advanced search

--- src/Search/AdvancedSearch.java
+++ src/Search/AdvancedSearch.java
@@ -3,42 +3,94 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import Course.Course;
+import Course.Lecture;package Search;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import Course.Course;
 import Course.Lecture;
 
 public class AdvancedSearch extends JPanel {
-   /* JLabel searchLabel;
-    JComboBox searchDropMenu;
-    JComboBox searchDropMenu2;
+    private JPanel display;
+    private Jpanel control;
+    private Scheduler schedule;
+    
     String[] searchOptions = {"Department", "Professor", "General Education"};
-    TextField text;
-    
-    public AdvancedSearch() {
-	searchLavel = new Label("Advanced Search");
-	searchDropMenu = new JComboBox(searchOptions);
-	searchDropMenu.addActionListener(new ActionListener(mainOptionList())); 
-	searchDropMenu2.addActionListener(new ActionListener(subOptionList()));
+
+    public AdvancedSearch(){
+	this.schedule = new Scheduler();
     }
-	
-    public void mainOptionList() {
-	//gets the selected item from searchDropMenu and passes it into showResults
-	String searchOption = (String)searchDropMenu.getSelectedItem();
-	public void actionPerformed(ActionEvent e) {
-	    "stub";
-	}
-    }
-    
-    public void subOptionList() {
-	//gets the selected item in searchDropMenu2 and then populates the page with the 
-	//results from this query
-	String searchOption = (String)searchDropMenu2.getSelectedItem();
-	public void actionPerformed(Action e) {
-	    "stub";
-	}
+    public AdvancedSearch(Scheduler s){
+	this.schedule = s;
     }
 
-    public static void main(String[] args){
-	new AdvancedSearch();
+    /**
+    Initializes the display
+    */
+    public void setDisplay(){
+	//Initialize panel
+	Jpanel panel = new JPanel();
+	panel.setPreferredSize(new Dimension(910,600));
+	panel.setLayout(new BorderLayout());
+
+	//make control panel
+	panel.add(this.getControl(), BorderLayout.NORTH);
+        panel.add(this.getCourses(), BorderLayout.SOUTH);
+        this.display = panel;
     }
-*/
+
+    /**
+     @return returns the full set display with both the control and course panels
+    */
+    public JPanel getDisplay(){
+        this.setDisplay();
+        return this.display;
+    }
+
+    /**
+     Sets the display panel and then returns it.
+     @return the display panel that includes a control panel that you can type a keyword into
+    */
+    public void setControl(){
+        JPanel controlPanel = new JPanel();
+        //Probably want the control panel to be a horizontal row of buttons
+        controlPanel.setPreferredSize(new Dimension(500,33));
+        controlPanel.setLayout(new GridLayout(1, 3));
+        JPanel[] panelHolder = new JPanel[3];
+        for(int i = 0; i<3; i++){
+            panelHolder[i] = new JPanel();
+            panelHolder[i].setBackground(new Color(184,194,247));
+            controlPanel.add(panelHolder[i]);
+        }
+
+	//make components
+       	JLabel searchLabel = new JLabel("Choose an option");
+	JRadioButton radioButtons = new JRadioButton[3];
+	for(int i=0;i<3;i++){
+	    radioButtons[i] = new JRadioButton(searchOptions[i]);
+	}
+	class populateOptions implements ActionListener{
+	    private JRadioButton button;
+	    private AdvancedSearch a;
+
+	    public populateOptions(JRadioButton button, AdvancedSearch a;){
+		this.button = button;
+		this.a = a;
+	    }
+
+	    //populates the next menu with items related to that search option 
+	    public void actionPerformed(ActionEvent e){
+		"Stub";
+	    }
+	}
+	panelHolder[0].add(searchLabel);
+        for(int i=0; i<3; i++){
+	    panelHolder[1].add(radioButtons[i]);
+	}
+    }
 }
+			  	       	       		    		      	       
+			       
+			       
+    
    
